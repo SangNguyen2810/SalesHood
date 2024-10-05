e5d2ff89110393aa66e44b74154c83af3f2771fa
Sang Nguyen <nguyenquangsang2810@gmail.com>
2024-10-05 10:32:34 +0700

    Add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home"
 import ToDoPage from "./pages/ToDoPage/ToDoPage";
 import "./App.css";
